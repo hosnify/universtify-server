6ebc71763ba928a23cdc60b465b1cc4a4a4acf11
hosnify <hosnify@gmail.com>
2021-07-02 18:14:52 +0200

    add get one semester

--- routes/semesterAPI.js
+++ routes/semesterAPI.js
@@ -7,13 +7,34 @@ const prisma = new PrismaClient();
 //GET all semesters
 router.get("/semesters", async (req, res) => {
   try {
-    const semester = await prisma.semester.findMany();
+    const semester = await prisma.semester.findMany({
+      include: {
+        Courses: true,
+        students: true,
+      },
+    });
     res.json(semester);
   } catch (err) {
     res.json({ error: "wrong data", errMsg: err });
   }
 });
 
+//GET one semester
+router.get("/semester/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const semester = await prisma.semester.findUnique({
+      where: { id: Number(id) },
+      include: {
+        Courses: true,
+        students: true,
+      },
+    });
+    res.json(semester);
+  } catch (err) {
+    res.json({ error: "wrong data", errMsg: err });
+  }
+});
 //creat semester
 router.post("/semester", async (req, res) => {
   const { type, year, coordinatorId } = req.body;
@@ -52,19 +73,21 @@ router.put("/semester/:id", async (req, res) => {
 });
 
 //update semester => add course to semester
-router.put("/semester/:semesterId/course/courseId", async (req, res) => {
+router.put("/semester/:semesterId/course/:courseId", async (req, res) => {
   const { semesterId, courseId } = req.params;
   try {
-    const updateSemester = await prisma.semester.update({
-      where: {
-        id: Number(semesterId),
-      },
-      data: {
-        Courses: {
-          connect: { id: Number(courseId) },
+    const updateSemester = await prisma.semester
+      .update({
+        where: {
+          id: Number(semesterId),
         },
-      },
-    });
+        data: {
+          Courses: {
+            connect: { id: Number(courseId) },
+          },
+        },
+      })
+      .Courses();
     res.json(updateSemester);
   } catch (err) {
     res.json({ error: "wrong data", errMsg: err });
